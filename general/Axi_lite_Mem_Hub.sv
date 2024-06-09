1f6e870a42d7075a7bcea1d3ffe19c0b047b4988
tlhatepy3 <3220103338@zju.edu.cn>
2024-06-09 15:38:40 +0800

    axi_lite part

--- general/Axi_lite_Mem_Hub.sv
+++ general/Axi_lite_Mem_Hub.sv
@@ -12,6 +12,8 @@ module Axi_lite_Mem_Hub #(
     input rstn,
     AXI_ift.Slave master0,
     AXI_ift.Slave master1,
+    AXI_ift.Slave master2,
+    AXI_ift.Slave master3,
     AXI_ift.Master slave0,
     AXI_ift.Master slave1,
     AXI_ift.Master slave2
@@ -56,20 +58,26 @@ module Axi_lite_Mem_Hub #(
         rvalid:0
     };
 
-    localparam IDLE=2'b00;
-    localparam MASTER0=2'b01;
-    localparam MASTER1=2'b10;
+    localparam IDLE=3'b000;
+    localparam MASTER0=3'b001;
+    localparam MASTER1=3'b010;
+    localparam MASTER2=3'b011;
+    localparam MASTER3=3'b100;
 
-    wire [AXI_ADDR_WIDTH-1:0] waddr [1:0];
+    wire [AXI_ADDR_WIDTH-1:0] waddr [2:0];
     assign waddr[0]=master0.Mw.awaddr;
     assign waddr[1]=master1.Mw.awaddr;
-    wire [1:0] wrequest;
+    assign waddr[2]=master2.Mw.awaddr;
+    assign waddr[3]=master3.Mw.awaddr;
+    wire [2:0] wrequest;
     assign wrequest[0]=master0.Mw.awvalid;
     assign wrequest[1]=master1.Mw.awvalid;
+    assign wrequest[2]=master2.Mw.awvalid; 
+    assign wrequest[3]=master3.Mw.awvalid;   
     wire [2:0] wismem [1:0];
     genvar i;
     generate
-        for(i=0;i<=1;i=i+1)begin:wismem_set
+        for(i=0;i<=3;i=i+1)begin:wismem_set
             assign wismem[i][0]=(waddr[i]<MEM0_END)&wrequest[i];
             assign wismem[i][1]=(MEM1_BEGIN<=waddr[i])&(waddr[i]<MEM1_END)&wrequest[i];
             assign wismem[i][2]=(MEM2_BEGIN<=waddr[i])&(waddr[i]<MEM2_END)&wrequest[i];
@@ -83,6 +91,8 @@ module Axi_lite_Mem_Hub #(
     assign wfinish[2]=slave2.Sw.bvalid;
     wire [2:0] wismaster0;
     wire [2:0] wismaster1;
+    wire [2:0] wismaster2;
+    wire [2:0] wismaster3;
     generate
         for(i=0;i<3;i=i+1)begin
             always@(posedge clk)begin
@@ -91,16 +101,21 @@ module Axi_lite_Mem_Hub #(
                 end else begin
                     case(axi_wtask[i])
                         IDLE:
-                        begin
-                            if(wismem[1][i])
+                        begin   // priority exists here, take dmmu > immu > other first
+                            if(wismem[3][i])
+                                axi_wtask[i]<=MASTER3;
+                            else if(wismem[2][i])
+                                axi_wtask[i]<=MASTER2;
+                            else if(wismem[1][i])
                                 axi_wtask[i]<=MASTER1;
                             else if(wismem[0][i])
                                 axi_wtask[i]<=MASTER0;
                         end
+                        // take them as circle first
                         MASTER0:
                         begin
-                            if(wfinish[i]&wismem[1][i])
-                                axi_wtask[i]<=MASTER1;
+                            if(wfinish[i]&wismem[3][i])
+                                axi_wtask[i]<=MASTER3;
                             else if(wfinish[i])
                                 axi_wtask[i]<=IDLE;
                         end
@@ -108,6 +123,20 @@ module Axi_lite_Mem_Hub #(
                         begin
                             if(wfinish[i]&wismem[0][i])
                                 axi_wtask[i]<=MASTER0;
+                            else if(wfinish[i])
+                                axi_wtask[i]<=IDLE;
+                        end
+                        MASTER2:
+                        begin
+                            if(wfinish[i]&wismem[1][i])
+                                axi_wtask[i]<=MASTER1;
+                            else if(wfinish[i])
+                                axi_wtask[i]<=IDLE;
+                        end
+                        MASTER3:
+                        begin
+                            if(wfinish[i]&wismem[2][i])
+                                axi_wtask[i]<=MASTER2;
                             else if(wfinish[i])
                                 axi_wtask[i]<=IDLE;
                         end
@@ -120,6 +149,8 @@ module Axi_lite_Mem_Hub #(
             end
             assign wismaster0[i]=axi_wtask[i]==MASTER0;
             assign wismaster1[i]=axi_wtask[i]==MASTER1;
+            assign wismaster2[i]=axi_wtask[i]==MASTER2;
+            assign wismaster3[i]=axi_wtask[i]==MASTER3;
         end
     endgenerate
             
@@ -127,6 +158,8 @@ module Axi_lite_Mem_Hub #(
         case(axi_wtask[0])
             MASTER0:slave0.Mw=master0.Mw;
             MASTER1:slave0.Mw=master1.Mw;
+            MASTER2:slave0.Mw=master2.Mw;
+            MASTER3:slave0.Mw=master3.Mw;
             default:slave0.Mw=dummy_axi.Mw;
         endcase
     end
@@ -135,6 +168,8 @@ module Axi_lite_Mem_Hub #(
         case(axi_wtask[1])
             MASTER0:slave1.Mw=master0.Mw;
             MASTER1:slave1.Mw=master1.Mw;
+            MASTER2:slave1.Mw=master2.Mw;
+            MASTER3:slave1.Mw=master3.Mw;
             default:slave1.Mw=dummy_axi.Mw;
         endcase
     end
@@ -143,6 +178,8 @@ module Axi_lite_Mem_Hub #(
         case(axi_wtask[2])
             MASTER0:slave2.Mw=master0.Mw;
             MASTER1:slave2.Mw=master1.Mw;
+            MASTER2:slave2.Mw=master2.Mw;
+            MASTER3:slave2.Mw=master3.Mw;
             default:slave2.Mw=dummy_axi.Mw;
         endcase
     end
@@ -165,17 +202,40 @@ module Axi_lite_Mem_Hub #(
         endcase
     end
 
+// mmu
+    always_comb begin
+        case(wismaster2)
+            3'b001:master2.Sw=slave0.Sw;
+            3'b010:master2.Sw=slave1.Sw;
+            3'b100:master2.Sw=slave2.Sw;
+            default:master2.Sw=dummy_axi.Sw;
+        endcase
+    end
+
+    always_comb begin
+        case(wismaster3)
+            3'b001:master3.Sw=slave0.Sw;
+            3'b010:master3.Sw=slave1.Sw;
+            3'b100:master3.Sw=slave2.Sw;
+            default:master3.Sw=dummy_axi.Sw;
+        endcase
+    end
+
     //------------------------------------------------
-    
-    wire [AXI_ADDR_WIDTH-1:0] raddr [1:0];
+
+    wire [AXI_ADDR_WIDTH-1:0] raddr [2:0];
     assign raddr[0]=master0.Mr.araddr;
     assign raddr[1]=master1.Mr.araddr;
-    wire [1:0] rrequest;
+    assign raddr[2]=master2.Mr.araddr;
+    assign raddr[3]=master3.Mr.araddr;
+    wire [2:0] rrequest;
     assign rrequest[0]=master0.Mr.arvalid;
     assign rrequest[1]=master1.Mr.arvalid;
-    wire [2:0] rismem [1:0];
+    assign rrequest[2]=master2.Mr.arvalid;
+    assign rrequest[3]=master3.Mr.arvalid;
+    wire [3:0] rismem [1:0];
     generate
-        for(i=0;i<=1;i=i+1)begin:rismem_set
+        for(i=0;i<=3;i=i+1)begin:rismem_set
             assign rismem[i][0]=(raddr[i]<MEM0_END)&rrequest[i];
             assign rismem[i][1]=(MEM1_BEGIN<=raddr[i])&(raddr[i]<MEM1_END)&rrequest[i];
             assign rismem[i][2]=(MEM2_BEGIN<=raddr[i])&(raddr[i]<MEM2_END)&rrequest[i];
@@ -189,6 +249,8 @@ module Axi_lite_Mem_Hub #(
     assign rfinish[2]=slave2.Sr.rvalid;
     wire [2:0] rismaster0;
     wire [2:0] rismaster1;
+    wire [2:0] rismaster2;
+    wire [2:0] rismaster3;
     generate
         for(i=0;i<3;i=i+1)begin
             always@(posedge clk)begin
@@ -198,14 +260,18 @@ module Axi_lite_Mem_Hub #(
                     case(axi_rtask[i])
                         IDLE:
                         begin
-                            if(rismem[1][i])
+                            if(rismem[3][i])
+                                axi_rtask[i]<=MASTER3;
+                            else if(rismem[2][i])
+                                axi_rtask[i]<=MASTER2;
+                            else if(rismem[1][i])
                                 axi_rtask[i]<=MASTER1;
                             else if(rismem[0][i])
                                 axi_rtask[i]<=MASTER0;
                         end
                         MASTER0:
                         begin
-                            if(rfinish[i]&rismem[1][i])
+                            if(rfinish[i]&rismem[3][i])
                                 axi_rtask[i]<=MASTER1;
                             else if(rfinish[i])
                                 axi_rtask[i]<=IDLE;
@@ -214,6 +280,20 @@ module Axi_lite_Mem_Hub #(
                         begin
                             if(rfinish[i]&rismem[0][i])
                                 axi_rtask[i]<=MASTER0;
+                            else if(rfinish[i])
+                                axi_rtask[i]<=IDLE;
+                        end
+                        MASTER2:
+                        begin
+                            if(rfinish[i]&rismem[1][i])
+                                axi_rtask[i]<=MASTER1;
+                            else if(rfinish[i])
+                                axi_rtask[i]<=IDLE;
+                        end
+                        MASTER3:
+                        begin
+                            if(rfinish[i]&rismem[2][i])
+                                axi_rtask[i]<=MASTER2;
                             else if(rfinish[i])
                                 axi_rtask[i]<=IDLE;
                         end
@@ -226,6 +306,8 @@ module Axi_lite_Mem_Hub #(
             end
             assign rismaster0[i]=axi_rtask[i]==MASTER0;
             assign rismaster1[i]=axi_rtask[i]==MASTER1;
+            assign rismaster2[i]=axi_rtask[i]==MASTER2;
+            assign rismaster3[i]=axi_rtask[i]==MASTER3;
         end
     endgenerate
             
@@ -233,6 +315,8 @@ module Axi_lite_Mem_Hub #(
         case(axi_rtask[0])
             MASTER0:slave0.Mr=master0.Mr;
             MASTER1:slave0.Mr=master1.Mr;
+            MASTER2:slave0.Mr=master2.Mr;
+            MASTER3:slave0.Mr=master3.Mr;
             default:slave0.Mr=dummy_axi.Mr;
         endcase
     end
@@ -241,6 +325,8 @@ module Axi_lite_Mem_Hub #(
         case(axi_rtask[1])
             MASTER0:slave1.Mr=master0.Mr;
             MASTER1:slave1.Mr=master1.Mr;
+            MASTER2:slave1.Mr=master2.Mr;
+            MASTER3:slave1.Mr=master3.Mr;
             default:slave1.Mr=dummy_axi.Mr;
         endcase
     end
@@ -249,6 +335,8 @@ module Axi_lite_Mem_Hub #(
         case(axi_rtask[2])
             MASTER0:slave2.Mr=master0.Mr;
             MASTER1:slave2.Mr=master1.Mr;
+            MASTER2:slave2.Mr=master2.Mr;
+            MASTER3:slave2.Mr=master3.Mr;
             default:slave2.Mr=dummy_axi.Mr;
         endcase
     end
@@ -268,6 +356,24 @@ module Axi_lite_Mem_Hub #(
             3'b010:master1.Sr=slave1.Sr;
             3'b100:master1.Sr=slave2.Sr;
             default:master1.Sr=dummy_axi.Sr;
+        endcase
+    end    
+
+    always_comb begin
+        case(rismaster2)
+            3'b001:master2.Sr=slave0.Sr;
+            3'b010:master2.Sr=slave1.Sr;
+            3'b100:master2.Sr=slave2.Sr;
+            default:master2.Sr=dummy_axi.Sr;
+        endcase
+    end    
+
+    always_comb begin
+        case(rismaster3)
+            3'b001:master3.Sr=slave0.Sr;
+            3'b010:master3.Sr=slave1.Sr;
+            3'b100:master3.Sr=slave2.Sr;
+            default:master3.Sr=dummy_axi.Sr;
         endcase
     end    
     
