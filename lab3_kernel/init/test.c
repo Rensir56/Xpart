6971bcfa54836a9ff7adb050961627c99e0e0af8
tlhatepy3 <3220103338@zju.edu.cn>
2024-06-11 20:18:26 +0800

    test

--- lab3_kernel/init/test.c
+++ lab3_kernel/init/test.c
@@ -6,12 +6,5 @@
 void test() {
     unsigned long record_time = 0; 
     while (1) {
-        unsigned long present_time;
-        __asm__ volatile("rdtime %[t]" : [t] "=r" (present_time) : : "memory");
-        present_time /= 10000000;
-        if (record_time < present_time) {
-            printk("kernel is running! Time: %lus\n", present_time);
-            record_time = present_time; 
-        }
     }
 }
